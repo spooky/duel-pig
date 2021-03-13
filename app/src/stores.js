df85228233aa034cdbc1d4ce0b3c9ffd192f918b
spooky <spooky9@poczta.onet.pl>
2021-03-13 20:18:12 +0100

    added exile to the map pool

--- app/src/stores.js
+++ app/src/stores.js
@@ -4,8 +4,8 @@ export const db = readable(
     null,
     function start(set) {
         set({
-            // maps: ['awoken', 'blood_covenant', 'blood_run', 'burial_chamber', 'church', 'corrupted_keep', 'deep_embrace', 'lockbox', 'longest_yard', 'ruins_of_sarnath', 'tempest_shrine', 'the_molten_falls', 'tower_of_koth', 'vale_of_pnath'],
-            maps: ['awoken', 'blood_covenant', 'blood_run', 'corrupted_keep', 'deep_embrace', 'ruins_of_sarnath', 'the_molten_falls', 'tower_of_koth', 'vale_of_pnath'],
+            // maps: ['awoken', 'blood_covenant', 'blood_run', 'burial_chamber', 'church', 'corrupted_keep', 'deep_embrace', 'exile', 'lockbox', 'longest_yard', 'ruins_of_sarnath', 'tempest_shrine', 'the_molten_falls', 'tower_of_koth', 'vale_of_pnath'],
+            maps: ['awoken', 'blood_covenant', 'blood_run', 'corrupted_keep', 'deep_embrace', 'exile', 'ruins_of_sarnath', 'the_molten_falls', 'tower_of_koth', 'vale_of_pnath'],
             champs: ['anarki', 'athena', 'bj', 'clutch', 'death_knight', 'doom', 'eisen', 'galena', 'keel', 'nyx', 'ranger', 'scalebearer', 'slash', 'sorlag', 'strogg', 'visor']
         });
 
